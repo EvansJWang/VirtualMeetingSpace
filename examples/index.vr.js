672d30a626e8310e4a29eb6307e8b61cd79f4008
Evans Wang <evansjwang@gmail.com>
2017-07-26 12:45:26 -0400

    Establish React-VR with 3 window components

--- examples/index.vr.js
+++ examples/index.vr.js
@@ -8,23 +8,55 @@ import CnCardContent from '../components/card/card-content';
 import CnCardFooter from '../components/card/card-footer';
 import CnCardHeader from '../components/card/card-header';
 import CnRow from '../components/core/row';
+import CnNavBar from '../components/navbar/navbar';
+import CnNavItem from '../components/navbar/nav-item';
 
 export default class Example extends React.Component {
     render() {
         return (
-            <CnContainer>
+          <View>
+            <CnContainer direction="left">
+              <CnNavBar>
+                <CnNavItem>
+                  Navigation bar item tester
+                </CnNavItem>
+                <CnNavItem>
+                  This is the second item
+                </CnNavItem>
+              </CnNavBar>
                 <View row flex>
                     <View vcenter hcenter bg="red" flex>
-                        <Text style={{color: '#000'}}>dsadas</Text>
+                        <Text style={{color: '#000'}}>Aaron Website</Text>
                     </View>
                     <View vcenter hcenter bg="green" flex={2}>
-                        <Text style={{color: '#000'}}>dsadas</Text>
+                        <Text style={{color: '#000'}}>Aaron Resume</Text>
                     </View>
                     <View vcenter hcenter bg="yellow">
-                        <Text style={{color: '#000'}}>dsadas</Text>
+                        <Text style={{color: '#000'}}>Aaron  Favorite pictures</Text>
                     </View>
                 </View>
             </CnContainer>
+            <CnContainer direction="front">
+                <View row flex>
+                    <View vcenter flex>
+                        <Text style={{color: '#000'}}> Ba-Blam! </Text>
+                    </View>
+                </View>
+            </CnContainer>
+            <CnContainer direction="right">
+                <View row flex>
+                    <View vcenter hcenter bg="blue" flex>
+                        <Text style={{color: '#000'}}>Evans' Cool stuff</Text>
+                    </View>
+                    <View vcenter hcenter bg="white" flex={2}>
+                        <Text style={{color: '#000'}}>Another cool component</Text>
+                    </View>
+                    <View vcenter hcenter bg="pink">
+                        <Text style={{color: '#000'}}> I have friends </Text>
+                    </View>
+                </View>
+            </CnContainer>
+            </View>
         );
     }
 }
